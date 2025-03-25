e5d9ec406e2cabe90a0274525e9c1f88023aead5
talari30 <talarirahul30@gmail.com>
2025-03-25 01:49:26 -0400

    NEW: SIGNUP-CARD AND SIGNUP-PAGE

---
+++ crowdcrest-frontend/app/Signup/page.tsx
@@ -0,0 +1,18 @@
+import Signup from "@/modules/Signupcard";
+import {Pageheader} from "@/modules/Pageheader";
+import styles from "./Signup.module.css";
+
+
+export const SSignup=  ()=>{
+        return (
+            <div className={styles.container}>
+        <><div>
+                        <Pageheader />
+                </div><div className={styles.SSignup}>
+                                <Signup placeholder_uid="new username" placeholder_password="new password" />
+                        </div></>
+                        </div>
+        );
+
+}
+export default SSignup;
--- crowdcrest-frontend/modules/Signupcard/index.tsx
+++ crowdcrest-frontend/modules/Signupcard/index.tsx
@@ -4,28 +4,30 @@ import {Input} from "@/elements/Input";
 import {Button} from "@/elements/Button";
 import {Hlink} from "@/elements/link";
 import { JSX, ReactNode } from "react";
-import styles from "@/modules/logincard/logincard.module.css";
+import styles from "@/modules/Signupcard/Signupcard.module.css";
 interface LoginProps {
     placeholder_uid: string;
     placeholder_password: string;
     
   }
-const Login = (props: LoginProps): JSX.Element => {
+const Signup = (props: LoginProps): JSX.Element => {
     const { placeholder_uid, placeholder_password } = props;
   
     return (
-      <div className={styles.login}>
+      <div className={styles.signup}>
         <div className={styles.loginH}><Heading size={3} >Login</Heading></div>
         <div >
           <Text>Username</Text>
           <Input type="email" placeholder={placeholder_uid} id={"1"} name={"user"} />
           <Text>Password</Text>
           <Input type="password" placeholder={placeholder_password} id={"2"} name={"user2"} />
-          <Text>Not a user? <Hlink linker="http://localhost:3000/Signup">create</Hlink></Text>
+          <Text>Retype Password</Text>
+          <Input type="password" placeholder={placeholder_password} id={"2"} name={"user3"} />
+          
         </div>
       </div>
     );
   };
 
   
-  export default Login ;
+  export default Signup ;
